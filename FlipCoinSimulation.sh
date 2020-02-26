014d525a3a0d95bec9b67a33833c2d9755e30792
AshutoshMatal <ashutoshmatal33@gmail.com>
2020-02-26 14:53:16 +0530

    MODIFIED : Store the singlet combination in dictory and  also calculate percentage

--- FlipCoinSimulation.sh
+++ FlipCoinSimulation.sh
@@ -1,9 +1,26 @@
 #!/bin/bash -x
-echo " Welcome to  FilpCoin Simulation " 
-randomvalue=$((RANDOM%2))
-if(( $randomvalue == 0 ))
-then
-   echo "     Head     "
-else
-   echo "     Tail     "
-fi
+echo " Welcome to  FilpCoin Simulation "
+echo "Enter the fliping coin"
+read number
+declare -A Singlet 
+Head=0
+Tail=0
+for ((count=1; count<=$number; count++ ))
+do
+randomvalue=$((RANDOM%2+1))
+case $randomvalue in
+			1)
+   		 	Singlet[$count]="Head"
+				Head=$((Head+1))
+				;;
+			2)
+   			Singlet[$count]="Tail"
+				Tail=$((Tail+1))
+				;;
+esac
+done
+echo  " Singlet key and values result :${!Singlet[@]} :${Singlet[@]} "
+PercentOfHead=`echo "(($Head/2))" | bc -l`
+PercentOfTail=`echo "(($Tail/2))" | bc -l`
+echo "$PercentOfHead"
+echo "$PercentOfTail"
